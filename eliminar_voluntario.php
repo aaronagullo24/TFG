6012ed4854d13ac8b740046b4f78e4bd0ca88a1d
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-04-13 11:51:10 +0200

    eliminar voluntarios

--- eliminar_voluntario.php
+++ eliminar_voluntario.php
@@ -1,14 +1,17 @@
 <?php
 session_start();
-if (!isset($_SESSION['administrador'])) {
-    header("Location: login.php");
-}
 include_once "conectar.php";
-$conexion = conectar();
 $administrador = $_SESSION['administrador'];
+$conexion = conectar();
+
+$id_voluntario = $_REQUEST['voluntario'];
+
+
+$sentencia = $conexion->prepare("DELETE FROM voluntario WHERE Numero_socio=:Numero_socio");
+$resultado = $sentencia->execute(["Numero_socio" => $id_voluntario]);
+
 
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -83,84 +86,9 @@ $administrador = $_SESSION['administrador'];
         </div>
     </nav>
     <br>
-    <?php
-    function esBoraable($id, $conexion)
-    {
-        $sql = "SELECT * FROM parejas WHERE id_voluntario=:id_voluntario;";
-        $consulta = $conexion->prepare($sql);
-        $consulta->execute([":id_voluntario" => $id]);
-        if ($consulta->rowCount() < 1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    $sql = "SELECT * FROM voluntario";
-    $consulta = $conexion->prepare($sql);
-    $consulta->execute();
-
-
-    ?>
-    <input class="form-control col-md-3 light-table-filter" data-table="order-table" type="text" placeholder="buscar..">
     <br>
-    <br>
-    <table class="table table-hover order-table" id="tabla">
-        <thead class="thead-dark">
-            <tr>
-                <th style="width:180px; background-color: #5DACCD; color:#fff">Numero de Socio</th>
-                <th style="width:180px; background-color: #5DACCD; color:#fff">Nombre</th>
-                <th style="width:180px; background-color: #5DACCD; color:#fff">Titulacion</th>
-                <th style="width:180px; background-color: #5DACCD; color:#fff">Modificar</th>
-                <th style="width:180px; background-color: #5DACCD; color:#fff">Eliminar</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
 
-            while ($voluntario = $consulta->fetch(PDO::FETCH_OBJ)) {
-
-            ?>
-
-                <tr>
-                    <td><?php echo $voluntario->Numero_socio ?></td>
-                    <td><?php echo $voluntario->Nombre ?></td>
-                    <td><?php echo $voluntario->Titulacion ?></td>
-                    <td>
-
-                        <form action="modificar_voluntarios_admin.php" method="post">
-                            <input type="hidden" id="voluntario" name="voluntario" value="<?php echo $voluntario->Numero_socio ?>">
-                            <input type="submit" class="btn btn-success" value="Modificar">
-                        </form>
-                    </td>
-
-                    <td>
-                        <?php
-                        if (esBoraable($voluntario->Numero_socio, $conexion) == true) {
-                        ?>
-                            <form action="eliminar_voluntario.php" method="post">
-                                <input type="hidden" id="voluntario" name="voluntario" value="<?php echo $voluntario->Numero_socio ?>">
-                                <input type="submit" class="btn btn-danger" value="Eliminar">
-                            </form>
-                        <?php
-                        } else {
-                        ?>
-                            <div class="alert alert-warning alert-dismissable"> 
-                                El voluntario tiene a un dependiente a su cargo, debes deshacer la pareja
-                            </div>
-                        <?php
-                        }
-                        ?>
-                    </td>
-                <?php } ?>
-
-
-
-                </tr>
-        </tbody>
-    </table>
-
+    <div class="alert alert-success">Eliminador Correctamente</div>
 </body>
 
 </html>
-<?php
