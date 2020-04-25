54e8562d5eb20698ed498b433cf5dc4af58477ad
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-04-25 12:33:13 +0200

    calendario datos de base de datos

--- calendario.php
+++ calendario.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+include_once "conectar.php";
+$conexion = conectar();
+
+$sql = "SELECT * FROM calendarios";
+$consulta = $conexion->prepare($sql);
+$consulta->execute();
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,6 +23,31 @@
     <script src="fullcalendar/lib/moment.min.js"></script>
     <script src="fullcalendar/fullcalendar.js"></script>
 
+    <script>
+        $(document).ready(function() {
+            $('#calendar').fullCalendar({
+                events: [
+                    <?php
+                    while ($calendario = $consulta->fetch(PDO::FETCH_OBJ)) {
+                    ?> {
+
+                            id: "<?php echo $calendario->id ?>",
+                            title: "<?php echo $calendario->title ?>",
+                            start: "<?php echo $calendario->start ?>",
+                            end: "<?php echo $calendario->end ?>",
+                            url: "<?php echo $calendario->url ?>",
+                            color: " <?php echo $calendario->color ?>",
+                            editable: "<?php echo $calendario->editable ?>"
+
+                        },
+                    <?php
+                    }
+                    ?>
+                ]
+            })
+        });
+    </script>
+
 </head>
 
 <body>
@@ -19,13 +56,7 @@
 
     </div>
 
-    <script>
-        $(document).ready(function() {
-            $('#calendar').fullCalendar({
-            })
 
-        });
-    </script>
 </body>
 
 </html>
