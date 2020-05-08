3e0023517f2cf31839be73f203996a3c17afb7dc
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-05-08 11:36:55 +0200

    cambio barra navegacion

--- perfil_dependiente.php
+++ perfil_dependiente.php
@@ -53,119 +53,131 @@ $dependiente = $consulta->fetch(PDO::FETCH_OBJ);
             </div>
         </div>
     </nav>
-    <div class="container">
-        <br>
-        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
-            Eliminar Pareja
-        </button>
-        <hr>
+    <?php
+    $sql = "SELECT * FROM parejas WHERE id_voluntario=:id_voluntario";
+    $consulta = $conexion->prepare($sql);
+    $consulta->execute(["id_voluntario" => $voluntario->Numero_socio]);
+    $consulta_dependientes = $consulta->fetch(PDO::FETCH_OBJ);
 
-        <div class="card bg-light">
-            <article class="card-body mx-auto" style="max-width: 400px;">
-                <h4 class="card-title mt-3 text-center">Perfil de <?php echo $dependiente->Nombre ?></h4>
+    if ($consulta->rowCount() == 0) {
+    ?>
+        <div class="alert alert-success mt-3">Podra ver el perfil de su dependiente cuando tenga uno a su cargo</div>
+    <?php
+    } else {
+    ?>
+        <div class="container">
+            <br>
+            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                Eliminar Pareja
+            </button>
+            <hr>
 
-                <form>
-                    <div class="input-group input-group-lg">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+            <div class="card bg-light">
+                <article class="card-body mx-auto" style="max-width: 400px;">
+                    <h4 class="card-title mt-3 text-center">Perfil de <?php echo $dependiente->Nombre ?></h4>
+
+                    <form>
+                        <div class="input-group input-group-lg">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+                            </div>
+
+                            <input id="Nombre" name="Nombre" class="form-control input-lg" aria-describedby="inputGroup-sizing-lg" placeholder="Nombre completo" type="text" value="<?php echo $dependiente->Nombre ?>" disabled>
+
+                            <div id="nombre"></div>
+                        </div>
+                        <br>
+
+                        <div class="input-group input-group-lg">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+                            </div>
+                            <input name="correo" id="correo" class="form-control" placeholder="Correo Electronico" type="email" value="<?php echo $dependiente->Correo ?>" disabled>
+                            <div id="Correo"></div>
+                        </div>
+                        <br>
+                        <div class="input-group input-group-lg">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                            </div>
+                            <input class="form-control" id="password" placeholder="Create password" type="password" value="<?php echo $dependiente->Password ?>" disabled>
+                            <div id="Password"></div>
+                        </div>
+                        <br>
+
+                        <div class="input-group mb-8">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="inputGroupSelect01">Provincia en la que vive: </label>
+                            </div>
+
+                            <select name="provinciaList" id="provinciaList" class="custom-select" disabled>
+                                <option><?php echo $dependiente->Provincia ?></option>
+
+                            </select>
+                            <div id="provincia"></div>
+
+                            <br>
+                            <br>
+                        </div>
+                        <div class="input-group mb-8">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="inputGroupSelect01">Localidad en la que vive: </label>
+                            </div>
+                            <select name="localidadList" id="localidadList" class="form-control" disabled>
+                                <option><?php echo $dependiente->Localidad ?></option>
+                            </select> <span id="advice"> </span>
+                            <div id="localidad"></div>
+                        </div>
+                        <br>
+
+
+                        <div class="form-group">
+                            <label for="start">Fecha de nacimiento:</label>
+
+                            <input type="date" id="fecha_nacimiento" name="trip-start" value="<?php echo $dependiente->Fecha_nacimiento ?>" disabled>
                         </div>
 
-                        <input id="Nombre" name="Nombre" class="form-control input-lg" aria-describedby="inputGroup-sizing-lg" placeholder="Nombre completo" type="text" value="<?php echo $dependiente->Nombre ?>" disabled>
 
-                        <div id="nombre"></div>
-                    </div>
-                    <br>
-
-                    <div class="input-group input-group-lg">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
-                        </div>
-                        <input name="correo" id="correo" class="form-control" placeholder="Correo Electronico" type="email" value="<?php echo $dependiente->Correo ?>" disabled>
-                        <div id="Correo"></div>
-                    </div>
-                    <br>
-                    <div class="input-group input-group-lg">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
-                        </div>
-                        <input class="form-control" id="password" placeholder="Create password" type="password" value="<?php echo $dependiente->Password ?>" disabled>
-                        <div id="Password"></div>
-                    </div>
-                    <br>
-
-                    <div class="input-group mb-8">
-                        <div class="input-group-prepend">
-                            <label class="input-group-text" for="inputGroupSelect01">Provincia en la que vive: </label>
-                        </div>
-
-                        <select name="provinciaList" id="provinciaList" class="custom-select" disabled>
-                            <option><?php echo $dependiente->Provincia ?></option>
-
-                        </select>
-                        <div id="provincia"></div>
 
                         <br>
-                        <br>
-                    </div>
-                    <div class="input-group mb-8">
-                        <div class="input-group-prepend">
-                            <label class="input-group-text" for="inputGroupSelect01">Localidad en la que vive: </label>
+                        <div class="form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+                            </div>
+                            <textarea class="form-control" rows="3" id="dependencia" placeholder="<?php echo $dependiente->Necesidad ?>" disabled></textarea>
+
+                            <div id="fecha"></div>
+
                         </div>
-                        <select name="localidadList" id="localidadList" class="form-control" disabled>
-                            <option><?php echo $dependiente->Localidad ?></option>
-                        </select> <span id="advice"> </span>
-                        <div id="localidad"></div>
-                    </div>
-                    <br>
+                    </form>
+                </article>
+            </div>
 
-
-                    <div class="form-group">
-                        <label for="start">Fecha de nacimiento:</label>
-
-                        <input type="date" id="fecha_nacimiento" name="trip-start" value="<?php echo $dependiente->Fecha_nacimiento ?>" disabled>
-                    </div>
-
-
-
-                    <br>
-                    <div class="form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">¿Esta seguro que desea eliminar a su pareja?</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
                         </div>
-                        <textarea class="form-control" rows="3" id="dependencia" placeholder="<?php echo $dependiente->Necesidad ?>" disabled></textarea>
-
-                        <div id="fecha"></div>
-
-                    </div>
-                </form>
-            </article>
-        </div>
-
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">¿Esta seguro que desea eliminar a su pareja?</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        Si elimina a su voluntario este ya no sera su pareja y tendra que ponerse en contacto con otro
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <form action="eliminar_pareja_voluntario.php">
-                            <input type="hidden" id="voluntario" name="voluntario" value="<?php echo $voluntario->Numero_socio ?>">
-                            <input type="hidden" id="dependiente" name="dependiente" value="<?php echo $dependiente->Numero_socio ?>">
-                            <input type="submit" class="btn btn-danger" value="ELIMINAR">
-                        </form>
+                        <div class="modal-body">
+                            Si elimina a su voluntario este ya no sera su pareja y tendra que ponerse en contacto con otro
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <form action="eliminar_pareja_voluntario.php">
+                                <input type="hidden" id="voluntario" name="voluntario" value="<?php echo $voluntario->Numero_socio ?>">
+                                <input type="hidden" id="dependiente" name="dependiente" value="<?php echo $dependiente->Numero_socio ?>">
+                                <input type="submit" class="btn btn-danger" value="ELIMINAR">
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
+            <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+            <script src="js/bootstrap.min.js"></script>
         </div>
-        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-        <script src="js/bootstrap.min.js"></script>
-
-    </div>
+    <? } ?>
