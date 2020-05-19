6f5ada15a3871b176bba8a027af10c1a47369caa
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-05-19 10:09:54 +0200

    cabecera dependientes cambiada

--- emergencia_dependientes.php
+++ emergencia_dependientes.php
@@ -32,6 +32,7 @@ if ($dependiente->voluntario != null) {
 if (!isset($_SESSION['dependiente'])) {
     header("Location: login.php");
 }
+include_once("funciones.php");
 $dependiente = $_SESSION['dependiente'];
 $nombre = $dependiente->Nombre;
 ?>
@@ -50,33 +51,9 @@ $nombre = $dependiente->Nombre;
 
 <body style="background-color: aquamarine;">
 
-    <!-- menú de navegación -->
-    <nav class="navbar navbar-inverse bg-inverse navbar-toggleable-sm sticky-top">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="inicio_dependientes
-        .php">
-            <img src="resources/logo.png" width="30" height="30" class="d-inline-block align-top" alt="Logo Bootstrap">
-            <?php
-            echo $nombre;
-            ?>
-            </p> </a>
-        <div class="collapse navbar-collapse " id="navbarTogglerDemo01">
-            <div class="navbar-nav mr-auto ml-auto text-center">
-                <a class="nav-item nav-link " href="solicitar_dependientes.php" mark>Solicitar</a>
-                <a class="nav-item nav-link" href="ver_perfil_dependientes.php">Perfil</a>
-                <a class="nav-item nav-link " href="calendario_dependiente.php">Calendario</a>
-                <a class="nav-item nav-link " href="chat_voluntario.php">Chat</a>
-                <a class="nav-item nav-link " href="perfil_pareja_dependiente.php">Perfil del Voluntario</a>
-                <a class="nav-item nav-link text-danger" href="emergencia_dependientes.php">Emergencias</a>
-            </div>
-            <div class="d-flex flex-row justify-content-center">
-
-                <a href="login.php" class="btn btn-outline-danger">Cerrar sesion</a>
-            </div>
-        </div>
-    </nav>
+    <?php
+    dependiente($nombre);
+    ?>
     <div align="center" class="mt-3">
         <a href="tel:+34684105254"><img src="resources/sos.jpg" /></a>
 
