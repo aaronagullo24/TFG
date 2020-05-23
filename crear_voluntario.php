3f60711911f33aff3fd869e625e394e277df49e8
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-05-23 11:50:17 +0200

    cambios visuales

--- crear_voluntario.php
+++ crear_voluntario.php
@@ -15,17 +15,17 @@ include_once "cabecera.php";
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Oldver</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.css">
 
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/AjaxCode.js"></script>
     <script src="js/admin_voluntario.js"></script>
     <script src="js/mi_libreriaAjax.js"></script>
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
 </head>
 
 <body style="background-color: #4FD53C;">
--- eliminar_voluntario.php
+++ eliminar_voluntario.php
@@ -5,7 +5,7 @@ $administrador = $_SESSION['administrador'];
 $conexion = conectar();
 
 $id_voluntario = $_REQUEST['voluntario'];
-
+include_once "cabecera.php";
 
 $sentencia = $conexion->prepare("DELETE FROM voluntario WHERE Numero_socio=:Numero_socio");
 $resultado = $sentencia->execute(["Numero_socio" => $id_voluntario]);
@@ -28,67 +28,15 @@ $resultado = $sentencia->execute(["Numero_socio" => $id_voluntario]);
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 
-<body style="background-color: aquamarine;">
+<body style="background-color: #4FD53C;">
 
-    <!-- menú de navegación -->
-    <nav class="navbar navbar-inverse bg-inverse navbar-toggleable-sm sticky-top">
-
-        <div class="navbar-header">
-
-            <a class="navbar-brand" href="inicio_administrador.php">
-                <img src="resources/logo.png" width="30" height="30" class="d-inline-block align-top" alt="Logo Bootstrap"> Administrador
-                <?php
-                echo $administrador->nombre;
-                ?>
-                </p> </a>
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                <span class="sr-only">Desplegar navegación</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-
-        </div>
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
-            <ul class="nav navbar-nav">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        Crear
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a href="crear_voluntario.php">Crear Voluntario</a></li>
-                        <li><a href="crear_dependiente.php">Crear Dependiente</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <ul class="nav navbar-nav">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        Modificar
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a href="modificar_voluntario.php">Modificar Voluntario</a></li>
-                        <li><a href="modificar_dependiente.php">Modificar Dependiente</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <ul class="nav navbar-nav">
-                <li><a href="#">Chat</a></li>
-            </ul>
-            <ul class="nav navbar-nav">
-                <li><a href="#">Parejas</a></li>
-            </ul>
-        </div>
-        <div class="d-flex flex-row justify-content-center">
-
-            <a href="login.php" class="btn btn-outline-danger">Cerrar sesion</a>
-        </div>
-        </div>
-    </nav>
+    <?php
+    administrador($administrador);
+    ?>
     <br>
     <br>
 
-    <div class="alert alert-success">Eliminador Correctamente</div>
+    <div class="alert alert-success text-center">Eliminador Correctamente</div>
 </body>
 
 </html>
